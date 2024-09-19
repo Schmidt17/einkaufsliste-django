e37cf7c043af41c6f8ecf45c0d90b574f0a8beaf
ah <andreas.hofacker@posteo.de>
2024-09-19 23:43:28 +0200

    Update django settings

--- einkaufsliste/settings.py
+++ einkaufsliste/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from django.core.management.utils import get_random_secret_key
 
 from django.conf.global_settings import STATIC_ROOT, STATICFILES_DIRS
 
@@ -22,12 +24,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_-phvv)9-pmd^6rm$d!x)jqj*zv%it)i631@^_tbqz=uz(=d)3'
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost'] + os.getenv('DJANGO_ALLOWED_HOSTS').split(',')
+
+DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
 
 
 # Application definition
